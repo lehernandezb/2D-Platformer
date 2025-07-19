b16763fa709d3fb59ac603fa9efb1e21af5bab4d
Luis <luis.hernandez1@ucalgary.ca>
2025-07-18 19:44:53 -0600

    loading assets

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,17 @@
-import {k} from "./kaboomctx";
+import {k} from "./kaboomCtx";
 
-async function gameSetup() {}
+async function gameSetup() {
+    k.loadSprite("assets", "./spookyCharacter.png", {
+        sliceX: 9,
+        sliceY: 5,
+        anims: {
+            spookIdle: 0,
+            spookShooting: 1,
+            spookLow: 2,
+            spookBeam: {from: 10, to: 17, speed: 15, loop: true},
+            guyWalk: {from: 19, to: 20, speed: 4, loop: true}
+        }
+    });
+}
 
 gameSetup();
