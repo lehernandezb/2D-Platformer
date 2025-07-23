a496fca71c2345eb12d7b36fe4bc4193c1ead2dc
Luis <luis.hernandez1@ucalgary.ca>
2025-07-23 01:52:36 -0600

    finshed collide method and added comments ( will need to add variables later since it is currently hard coded)

--- src/entities.ts
+++ src/entities.ts
@@ -1,7 +1,21 @@
 import type { GameObj, KaboomCtx } from "kaboom";
 import { scale } from "./constants";
 
+// Reducing magic numbers
+const targetOpacityAfterHit: number = 0;
+const targetOpacityWhenRecovering = 1;
+const timingOfOpacityChange: number = 0.05;
+
+/**
+ * Method to make player and their logic
+ * 
+ * @param k KaboomCtx
+ * @param posx postion of player on x axis
+ * @param posy postion of player on y axis
+ */
 export function makePlayer(k : KaboomCtx, posx : number, posy : number) {
+
+    // Player object
     const player = k.make([
         k.sprite("assets", {anim : "spookIdle"}),
         k.area({shape : new k.Rect(k.vec2(4,5.9), 8 , 10)}),
@@ -21,7 +35,10 @@ export function makePlayer(k : KaboomCtx, posx : number, posy : number) {
         
     ]);
 
+    // Player collide physics
     player.onCollide("enemy", async (enemy: GameObj) => {
+
+        // player runs out of shooting gas
         if (player.isShooting && player.isEmpty) {
             player.isShooting = false;
             k.destroy(enemy);
@@ -29,10 +46,31 @@ export function makePlayer(k : KaboomCtx, posx : number, posy : number) {
             return;
         }
 
+        // Player dies
         if (player.hp() === 0) {
             k.destroy(player);
-            k.go("level-2");
+            k.go("level-1");
             return;
         }
+
+        // Player hurt
+        player.hurt();
+
+        // Player flash effect
+        await k.tween(
+            player.opacity,
+            targetOpacityAfterHit,
+            timingOfOpacityChange,
+            (val) => (player.opacity = val),
+            k.easings.linear
+        );
+        await k.tween(
+            player.opacity,
+            targetOpacityWhenRecovering,
+            timingOfOpacityChange,
+            (val) => (player.opacity = val),
+            k.easings.linear
+        );
+
     })
 }
